50a547c46e98170c7a02267655d35a87f463449f
Taylor Amy <taylorsamy@users.noreply.github.com>
2020-08-11 21:38:04 -0600

    Added video for AnimeNight

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const mysql = require('mysql');
 
 const indexRouter = require('./routes/index');
+const animeNightRouter = require('./routes/animenight')
 
 const app = express();
 
@@ -51,6 +52,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/animenight/', animeNightRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
